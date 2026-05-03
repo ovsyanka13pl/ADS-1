96153d4f7607c5e8e014dab970d2a5ac1f15eb0e
Aliaksei Huryanchyk <alexei.huryanchyk@synder.com>
2026-05-03 09:10:16 +0200

    Removed Node_2

--- src/main/java/pl/ovsyanka13/linked_list_2/Main.java
+++ src/main/java/pl/ovsyanka13/linked_list_2/Main.java
@@ -21,14 +21,14 @@ public class Main {
         System.out.println();
         System.out.println("-----------------------");
         LinkedList2_2 linkedList2_2 = new LinkedList2_2();
-        linkedList2_2.addInTail(new Node_2(1));
-        linkedList2_2.addInTail(new Node_2(2));
-        linkedList2_2.addInTail(new Node_2(3));
-        linkedList2_2.addInTail(new Node_2(4));
-        linkedList2_2.addInTail(new Node_2(5));
-        linkedList2_2.addInTail(new Node_2(6));
-        linkedList2_2.addInTail(new Node_2(7));
-        Node_2 current_2 = linkedList2_2.head;
+        linkedList2_2.addInTail(new Node(1));
+        linkedList2_2.addInTail(new Node(2));
+        linkedList2_2.addInTail(new Node(3));
+        linkedList2_2.addInTail(new Node(4));
+        linkedList2_2.addInTail(new Node(5));
+        linkedList2_2.addInTail(new Node(6));
+        linkedList2_2.addInTail(new Node(7));
+        Node current_2 = linkedList2_2.head;
         while (current_2 != null) {
             System.out.print(current_2.value + "-->");
             current_2 = current_2.next;
